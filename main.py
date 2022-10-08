2b029b16e2c44d70e5865c73fc1cd7e8fb1ae5dd
Dipti-24 <115351486+Dipti-24@users.noreply.github.com>
2022-10-08 18:40:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+# gen random no within range
+number = random.randint(0,9)
+print(number)
+# gen random no
+n = random.random()
+print(n)
+# gen random list and append it one by one
+l=[]
+for i in range(0,8):
+    n=random.randint(1,97)
+    l.append(n)
+print(l)
