71a592b79fa15e4ada803f4d676068deefb73207
Dipti-24 <115351486+Dipti-24@users.noreply.github.com>
2022-10-15 10:53:23 +0530

    Add files via upload

---
+++ global.py
@@ -0,0 +1,8 @@
+l=10
+def fun():
+    global l
+    l=20
+    m=9
+fun()
+print(l)
+print(m)
---
+++ imp.py
@@ -0,0 +1,17 @@
+w = lambda x,y:x-y
+print(w(9,8))
+
+lis =["d" ,"i","p"]
+a="".join(lis)
+print(a)
+
+n=["3","4","3"]
+#for i in range(len(n)):
+ #   n[i]=int(n[i])
+#n[2]=n[2]+1
+#print(n[2])
+#using map
+n=list(map(int,n))
+print(n)
+
+    
